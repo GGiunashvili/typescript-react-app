b19c1b7fc9fc65a16aad6e84e0186265cd1f9dfb
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2024-11-09 00:41:40 +0400

    input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 // import Quiz from "./componens/Quiz";
 // import MyQuiz from "./componens/MyQuiz";
-// import Todo from "./componens/Todo";
-import State from "./componens/State";
-import { useState } from "react";
-import { sculptureList } from "./componens/Data";
+// // import Todo from "./componens/Todo";
+// import State from "./componens/State";
+// import { useState } from "react";
+// import { sculptureList } from "./componens/Data";
+import Frm from "./componens/Frm";
 
 function App() {
-  const [index, setIndex] = useState(0); // Correct use of useState with parentheses
-  function handleClick() {
-    setIndex((index + 1) % sculptureList.length);
-  }
+  // const [index, setIndex] = useState(0); // Correct use of useState with parentheses
+  // function handleClick() {
+  //   setIndex((index + 1) % sculptureList.length);
+  // }
   return (
     <>
       {/* <MyQuiz /> */}
       {/* <Todo /> */}
       {/* <Form /> */}
-      <State index={index} handleClick={handleClick} />
-      <State index={index} handleClick={handleClick} />
+      {/* <State index={index} handleClick={handleClick} />
+      <State index={index} handleClick={handleClick} /> */}
+      <Frm />
     </>
   );
 }
